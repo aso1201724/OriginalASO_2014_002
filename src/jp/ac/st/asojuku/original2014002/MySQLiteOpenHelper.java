657a89b53c33a593b5a88a11f55a587733fc4034
student <student@PC111S08.fgakusei.local>
2014-06-06 10:44:34 +0900

    レイアウト　池田　コミット

---
+++ src/jp/ac/st/asojuku/original2014002/MySQLiteOpenHelper.java
@@ -0,0 +1,43 @@
+package jp.ac.st.asojuku.original2014002;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+
+	/**
+	 * @param context 呼び出しコンテキスト
+	 * @param name 利用ＤＢ名
+	 * @param factory　カーソルファクトリー
+	 * @param varsion　ＤＢバージョン
+	 */
+
+	public MySQLiteOpenHelper(Context context) {
+
+		super(context, "20140021201724.sqlite3", null, 1);
+
+	}
+
+
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		// TODO 自動生成されたメソッド・スタブ
+
+		db.execSQL("CREATE TABLE IF NOT EXISTS " +
+					"Hitokoto (_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, phrase TEXT)");
+		//尋ねに行き、なければCreateが実行されあれば普通に開かれる
+	}
+
+
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO 自動生成されたメソッド・スタブ
+		db.execSQL("drop table Hitokoto");
+		onCreate(db);
+
+	}
+
+}
